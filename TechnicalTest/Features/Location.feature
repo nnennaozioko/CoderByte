16703e599c72c1a41222d7d723f4b22599583b79
Nelly <nelytino@yahoo.com>
2022-10-23 23:03:19 +0100

    files

---
+++ TechnicalTest/Features/Location.feature
@@ -0,0 +1,9 @@
+﻿Feature: Location test
+
+Scenario Outline: Make successful location information
+Given I make a request to get location information <countryCode>,<postCode>
+Then I verify the request status <isSuccessful>
+Examples: 
+| countryCode | postCode | isSuccessful |
+| AD          | AD100    | true         |
+| AT          | 96799    | false        |
---
+++ TechnicalTest/LocationClient.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechnicalTest.Service;
+
+namespace TechnicalTest
+{
+    public class LocationClient
+    {
+        private readonly RestService _resrService;
+        private readonly ConfigurationProvider _configurationProvider;
+        private RestResponse Response;
+
+        public LocationClient(RestService restService, ConfigurationProvider configurationProvider)
+        {
+            _resrService = restService;
+            _configurationProvider = configurationProvider;
+        }
+
+        public void GetLocationInformation(string countryCode, string postCode)
+        {
+            var url = _configurationProvider.GetUrl();
+        }
+
+        public void VerifyRequestStatus(string isSuccessful)
+        {
+
+        }
+    }
+}
+
